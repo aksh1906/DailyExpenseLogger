c4efb32315f085229bbcc72a510ce06b3aac54fd
aksh1906 <akshat.sharma1906@gmail.com>
2018-10-27 22:57:01 +0530

    Added CalendarView and FloatingActionMenu to the main activity layout

--- app/src/main/java/com/akshatsharma/dailyexpenselogger/MainActivity.java
+++ app/src/main/java/com/akshatsharma/dailyexpenselogger/MainActivity.java
@@ -2,12 +2,18 @@ package com.akshatsharma.dailyexpenselogger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendarView = findViewById(R.id.cv_calendar);
+        calendarView.setDate(System.currentTimeMillis(), false, true);
     }
 }
